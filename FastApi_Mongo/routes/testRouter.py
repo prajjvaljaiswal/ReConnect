a15df60985f5ec8ca82a6e1eeb7b222af114c804
PRAJJVAL JAISWAL <prajjaiswal158@gmail.com>
2025-02-16 01:42:09 +0530

    - put and delete added

--- FastApi_Mongo/routes/testRouter.py
+++ FastApi_Mongo/routes/testRouter.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from models.user import User
 from config.database import collection_name
-# from bson import ObjectId
+from bson import ObjectId
 from schema.schemas import list_serial
 
 testRouter = APIRouter()
@@ -15,3 +15,8 @@ async def get_users():
 async def create_user(user: User):
     collection_name.insert_one(dict(user))
     return {"data": "User created successfully"}
+
+@testRouter.put("/users/{id}")
+async def update_user(id: str, user: User):
+    collection_name.update_one({"_id": ObjectId(id)}, {"$set": dict(user)})
+    return {"data": "User updated successfully"}
